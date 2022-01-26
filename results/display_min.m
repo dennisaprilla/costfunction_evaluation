060b1d72cacf242ab97fb5e681b06a5338d2ca9d
DennisAprilla <dennis.ap.ch@gmail.com>
2022-01-26 12:21:57 +0100

    add simulation data

--- results/display_min.m
+++ results/display_min.m
@@ -1,18 +1,27 @@
 clear; close all;
 
-filename='test.mat';
+filename='allcf_10_1100.mat';
 load(filename);
-
 
 rmse = 1;
 gmm  = 2;
 gmm2 = 3;
 
-figure;
+middle = floor(length(r_z)/2);
+costfunctions_min_normalized = costfunctions_min - middle;
+costfunctions_min_magnitude  = sqrt(sum((costfunctions_min_normalized.^2),2));
+
+figure1 = figure('Name', 'Min Cost Function', 'Position', [50 350 1250 350]);
 for i=1:3
     subplot(1,3,i);
-    scatter(costfunctions_min(:,1,i), costfunctions_min(:,2,i));
-    xlim([0 41]);
-    ylim([0 41]);
+    plot(costfunctions_min_normalized(:,1,i), costfunctions_min_normalized(:,2,i), '.r');
     grid on; axis equal;
+    xlim([-middle middle]);
+    ylim([-middle middle]);
+end
+
+figure2 = figure('Name', 'Distance from Zero', 'Position', [20 350 1250 350]);
+for i=1:3
+    subplot(1,3,i);
+    histogram(costfunctions_min_magnitude(:,:,i), 25);
 end

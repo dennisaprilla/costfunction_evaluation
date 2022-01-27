0dc3b8220c0e1180c9c332d4376665e0b620b6b1
DennisAprilla <dennis.ap.ch@gmail.com>
2022-01-27 09:28:07 +0100

    run simulation and get data

--- cfcheck_manualsample_partrials_all.m
+++ cfcheck_manualsample_partrials_all.m
@@ -32,8 +32,8 @@ Rs = eul2rotm(deg2rad(rs), 'ZYX');
 % change z translation to trgitanslation vector
 ts = [ zeros(2, length(t_z)); t_z];
 
-num_trials        = 1000;
-noise             = 1;
+num_trials        = 700;
+noise             = 3;
 num_costfunction  = 3;
 costfunctions_min = zeros(num_trials, 2, num_costfunction); 
 
@@ -110,4 +110,4 @@ for trial=1:num_trials
 
 end
 
-save('results\allcf_amode3_1.mat', 'costfunctions_min', 'r_z', 't_z');
+save('results\allcf_amode3_3.mat', 'costfunctions_min', 'r_z', 't_z');

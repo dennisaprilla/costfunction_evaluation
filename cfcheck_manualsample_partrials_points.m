5fa00ff55c0d1468aa6dc1b21d4cbce8db87079f
DennisAprilla <dennis.ap.ch@gmail.com>
2022-02-06 20:56:50 +0100

    run gmm 40

--- cfcheck_manualsample_partrials_points.m
+++ cfcheck_manualsample_partrials_points.m
@@ -30,10 +30,10 @@ ts = [ zeros(2, length(t_z)); t_z];
 
 noises            = [1 2 3];
 pointcounts       = [15 20 25 30];
-num_trials        = 750;
+num_trials        = 500;
 
-costfunction_name  = "gmm";
-costfunction_scale = 20;
+costfunction_name  = "rmse";
+costfunction_scale = 40;
 costfunctions_min  = ones(num_trials, 2, length(noises), length(pointcounts));
 
 for pointcount=1:length(pointcounts)
@@ -79,10 +79,16 @@ for pointcount=1:length(pointcounts)
                     U_breve_prime = Rs(:,:,current_z) * U_breve + ts(:,current_t);
                     scene_ptCloud = U_breve_prime';
                     
+                    %{
                     % GMM L2 Distance
                     scale = costfunction_scale * 1e-4;
                     [f,~] =  GaussTransform(double(model_ptCloud), double(scene_ptCloud), scale);
                     cf_temp(current_t) = -f;
+                    %}
+
+                    % RMSE
+                    [nearest_idx, nearest_dist] = knnsearch(scene_ptCloud, model_ptCloud);
+                    cf_temp(current_t) = mean(nearest_dist);
                     
                 end
                 
@@ -99,7 +105,7 @@ for pointcount=1:length(pointcounts)
         % end trials
         end
 
-        filename = sprintf('results\\tibia_%s_scale%d.mat', costfunction_name, costfunction_scale);
+        filename = sprintf('results\\tibia_%s_scale%d_%d.mat', costfunction_name, costfunction_scale, num_trials);
         save(filename, 'costfunctions_min', 'r_z', 't_z');
 
     % end noises

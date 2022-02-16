1a44eab36b1abc87153281da41f6b675ae267ae6
DennisAprilla <dennis.ap.ch@gmail.com>
2022-02-16 10:09:32 +0100

    rearrange files

--- results/concatmatrix.m
+++ results/concatmatrix.m
@@ -1,17 +1,15 @@
-% load('tibia_gmm_scale40_500.mat');
-% costfunctions_min1 = costfunctions_min;
-% load('tibia_gmm_scale40_150.mat');
-% costfunctions_min2 = costfunctions_min;
-% load('tibia_gmm_scale40_100.mat');
-% costfunctions_min3 = costfunctions_min;
-% 
-% costfunctions_min = cat(1, costfunctions_min1, costfunctions_min2, costfunctions_min3);
-% save('tibia_gmm_scale40.mat', 'costfunctions_min', 'r_z', 't_z');
-
-load('tibia_rmse_500.mat');
+load('tibia_gmm_scale10_600.mat');
 costfunctions_min1 = costfunctions_min;
-load('tibia_rmse_250.mat');
+load('tibia_gmm_scale10_150.mat');
 costfunctions_min2 = costfunctions_min;
 
 costfunctions_min = cat(1, costfunctions_min1, costfunctions_min2);
-save('tibia_rmse.mat', 'costfunctions_min', 'r_z', 't_z');
+save('tibia_gmm_scale10.mat', 'costfunctions_min', 'r_z', 't_z');
+
+% load('tibia_rmse_500.mat');
+% costfunctions_min1 = costfunctions_min;
+% load('tibia_rmse_250.mat');
+% costfunctions_min2 = costfunctions_min;
+% 
+% costfunctions_min = cat(1, costfunctions_min1, costfunctions_min2);
+% save('tibia_rmse.mat', 'costfunctions_min', 'r_z', 't_z');

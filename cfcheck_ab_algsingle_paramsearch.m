984b9cae8f7b13ceec7acc01c0cf3903c089c521
christieDA <d.a.christie@utwente.nl>
2022-03-21 17:56:41 +0100

    add ab simulation

--- cfcheck_ab_algsingle_paramsearch.m
+++ cfcheck_ab_algsingle_paramsearch.m
@@ -4,20 +4,22 @@ path_pointcloudregistration = 'D:\Documents\BELANDA\PhD Thesis\Code\MATLAB\point
 path_boneUSsimple           = 'D:\Documents\BELANDA\PhD Thesis\Code\MATLAB\boneUSsimple';
 path_gmmreg                 = 'D:\Documents\BELANDA\PhD Thesis\Code\cpp\gmmreg\MATLAB\GaussTransform';
 
-path_bone     = strcat(path_pointcloudregistration, filesep, 'data', filesep, 'bone');
-path_bmode    = strcat(path_boneUSsimple, filesep, 'outputs', filesep, 'usmeasurement_b');
+path_bone      = strcat(path_pointcloudregistration, filesep, 'data', filesep, 'bone');
+path_amode     = strcat(path_bone, filesep, filesep, 'amode_accessible_sim2');
+path_bmode     = strcat(path_boneUSsimple, filesep, 'outputs', filesep, 'usmeasurement_b');
 path_function1 = strcat(path_boneUSsimple, filesep, 'functions', filesep, 'experimental');
 path_function2 = strcat(path_boneUSsimple, filesep, 'functions', filesep, 'geometry');
-path_output   = 'results';
+path_output    = 'results';
 
 addpath(path_bone);
+addpath(path_amode);
 addpath(path_bmode);
 addpath(path_gmmreg);
 addpath(path_function1);
 addpath(path_function2);
 
-displaybone = true;
-displaycf   = true;
+displaybone = false;
+displaycf   = false;
 
 clear path_pointcloudregistration path_boneUSsimple path_gmmreg;
 
@@ -41,14 +43,30 @@ if(displaybone)
            U_breve(3,:), ...
            '.', 'Color', [0.7 0.7 0.7], ...
            'MarkerSize', 0.1, ...
-           'Tag', 'plot_bone_full');
+           'Tag', 'plot_bone');
     grid on; axis equal; hold on;
     xlabel('X'); ylabel('Y'); zlabel('Z');
 end
 
+%% Prepare the A-mode data
+filename_amodedata = sprintf('amode_tibia_15');
+filepath_amodedata = sprintf('%s%s%s.mat', path_amode, filesep, filename_amodedata);
+load(filepath_amodedata);
+Ua_pointcloud = vertcat(amode_all.Position)';
+
+% (for debugging only) show figure for sanity check
+if(displaybone)
+    grid on; axis equal; hold on;
+    plot3( axes1, ...
+           Ua_pointcloud(1,:), ...
+           Ua_pointcloud(2,:), ...
+           Ua_pointcloud(3,:), ...
+           'or', 'MarkerFaceColor', 'r', ...
+           'Tag', 'plot_bone');
+end
 
 %% Prepare the B-mode data
-filename_bmodedata = sprintf('usdata_b_1a');
+filename_bmodedata = sprintf('usdata_b_2b');
 filepath_bmodedata = sprintf('%s%s%s.mat', path_bmode, filesep, filename_bmodedata);
 load(filepath_bmodedata);
 Ub_pointcloud = bmode_simulation.pointcloud;
@@ -61,7 +79,7 @@ if(displaybone)
            Ub_pointcloud(:,2), ...
            Ub_pointcloud(:,3), ...
            'or', 'MarkerFaceColor', 'r', ...
-           'Tag', 'plot_bone_full');
+           'Tag', 'plot_bone');
 end
 
 %% Simulate Search Space
@@ -80,35 +98,41 @@ ts = [ zeros(2, length(t_z)); t_z];
 %% Simulation Setup
 
 % setup the noise constants
-noise_level     = 0;
+noise_level     = 1;
+noise_tconst    = 1;
 noise_skewconst = 0.025;
-noise_Rconst    = 1.50;
+noise_Rconst    = 1.25;
 % setup the noise
-noise_bin_t    = noise_level;
+noise_a        = noise_level;
+noise_bin_t    = noise_level*noise_tconst;
 noise_bin_s    = noise_level*noise_skewconst;
 noise_bex_R    = noise_level*noise_Rconst;
-noise_bex_t    = noise_level;
+noise_bex_t    = noise_level*noise_tconst;
 
 % setup the simulation trials config
-num_trials         = 1;
-costfunction_name  = "rmse";
-costfunction_scale = 10;
-use_boneportion    = true;
+costfunction_name       = "gmm";
+costfunction_scaleconst = 1e-4;
+costfunction_scales_a   = [20 30 40 50];
+costfunction_scales_b   = [10 20 30 40];
+costfunction_alphaconst = size(Ua_pointcloud, 1)/size(Ub_pointcloud, 1);
+costfunction_alphas     = [0.5 1.0 1.5];
+use_boneportion         = true;
+num_trials              = 100;
 
 % if use_boneportion is specified, we will use only the portion of the bone
 % instead of the whole bone. Portion is obtained from simulation toolbox 
 % (refer to file usmeasurement_b.m)
 if(use_boneportion)
     % get the bone portion
-    % U_breve = get_boneportion(bmode_simulation.portion, U_breve')';
-    U_breve = get_boneportion([0.125 0.225], U_breve')';
-    % U_breve = get_boneportion([0.25 0.35], U_breve')';
+    % U_breve_part = get_boneportion(bmode_simulation.portion, U_breve')';
+    % U_breve_part = get_boneportion([0.125 0.225], U_breve')';
+    U_breve_part = get_boneportion([0.25 0.35], U_breve')';
     % display it
     if(displaybone)
         plot3( axes1, ...
-               U_breve(1,:), ...
-               U_breve(2,:), ...
-               U_breve(3,:), ...
+               U_breve_part(1,:), ...
+               U_breve_part(2,:), ...
+               U_breve_part(3,:), ...
                '.', 'Color', [0.9290 0.6940 0.1250], ...
                'MarkerSize', 0.1, ...
                'Tag', 'plot_bone_full');
@@ -118,12 +142,33 @@ end
 % variable that will contains every global minimum of costfunction
 costfunctions_min  = ones(num_trials, 2);
 % naming the filename for result
-filename_simresult = sprintf('tibia_ab_%s_%d', costfunction_name, num_trials);
-    
+filepath = 'results\abmode_sim1';
+
+%% Simulation Start
+
+% loop for scale_a
+for costfunction_scale_a = 1:length(costfunction_scales_a)
+current_scale_a = costfunction_scales_a(costfunction_scale_a);
+scale_a = current_scale_a * costfunction_scaleconst;
+
+% loop for scale_b
+for costfunction_scale_b = 1:length(costfunction_scales_b)
+current_scale_b = costfunction_scales_b(costfunction_scale_b);
+scale_b = current_scale_b * costfunction_scaleconst;
+
+% loop for alpha
+for costfunction_alpha = 1:length(costfunction_alphas) 
+current_alpha = costfunction_alphas(costfunction_alpha);
+alpha         = current_alpha * costfunction_alphaconst;
+
 % for each config, do trials until the number of num_trials
 for trial=1:num_trials
 
     fprintf('trials: %d\n', trial);
+    
+    % add isotropic zero-mean gaussian noise to U, simulating noise measuremen
+    random_noise = -noise_a/ptCloud_scale + (noise_a/ptCloud_scale + noise_a/ptCloud_scale)*rand(3,size(Ua_pointcloud, 2));
+    Ua_noised = (Ua_pointcloud + random_noise)';
 
     % put internal and external noise to b-mode
     Ub_noised = Ub_pointcloud;
@@ -137,16 +182,19 @@ for trial=1:num_trials
     t3d_noise = noise_bex_t/ptCloud_scale;
     [Ub_noised, ~] = bmode_addnoise_external(Ub_noised, R_noise, t3d_noise);
 
-    % rename variable
-    model_ptCloud = Ub_noised;
-
     % show figure for sanity check
     if (displaybone)
         grid on; axis equal; hold on;
         plot3( axes1, ...
-               model_ptCloud(:,1), ...
-               model_ptCloud(:,2), ...
-               model_ptCloud(:,3), ...
+               Ua_noised(:,1), ...
+               Ua_noised(:,2), ...
+               Ua_noised(:,3), ...
+               'oy', ...
+               'Tag', 'plot_bone_full');
+        plot3( axes1, ...
+               Ub_noised(:,1), ...
+               Ub_noised(:,2), ...
+               Ub_noised(:,3), ...
                'oy', ...
                'Tag', 'plot_bone_full');
     end
@@ -167,19 +215,24 @@ for trial=1:num_trials
         for current_t = 1:loop_t
 
             % transform Ŭ with respected transformation
-            U_breve_prime = Rs(:,:,current_z) * U_breve + ts(:,current_t);
-            scene_ptCloud = U_breve_prime';
+            scene_ptCloud_a = ( Rs(:,:,current_z) * U_breve + ts(:,current_t) )';
+            scene_ptCloud_b = ( Rs(:,:,current_z) * U_breve_part + ts(:,current_t) )';
 
             if(strcmp(costfunction_name, "gmm"))
                 % GMM L2 Distance
-                scale = costfunction_scale * 1e-4;
-                [f,~] =  GaussTransform(double(model_ptCloud), double(scene_ptCloud), scale);
-                cf_temp(current_t) = -f;
+                [f_a,~] =  GaussTransform(double(Ua_noised), double(scene_ptCloud_a), scale_a);
+                [f_b,~] =  GaussTransform(double(Ub_noised), double(scene_ptCloud_b), scale_b);
+                cf_temp(current_t) = -(f_a + (alpha * f_b));
 
             elseif (strcmp(costfunction_name, "rmse"))
                 % RMSE
-                [nearest_idx, nearest_dist] = knnsearch(scene_ptCloud, model_ptCloud);
-                cf_temp(current_t) = mean(nearest_dist);
+                
+            elseif (strcmp(costfunction_name, "gmmrmse"))
+                % GMM and RMSE
+                
+            elseif (strcmp(costfunction_name, "rmsegmm"))
+                % RMSE and GMM
+                
             end
 
         end
@@ -206,10 +259,31 @@ for trial=1:num_trials
 
 % end trials    
 end
-    
-middle = ceil(loop_z/2);
-fprintf('rz_idx: %d\t\t\trt_idx: %d\n', costfunctions_min(1,1)-middle, costfunctions_min(1,2)-middle);
-fprintf('rz_est: %.2f deg\trt_est: %.2f mm\n', r_z(costfunctions_min(:, 1)), t_z(costfunctions_min(:, 2))*1000 );
+
+% save the details of the simulation
+simulation_config.scaleconst      = costfunction_scaleconst;
+simulation_config.scale_a         = current_scale_a;
+simulation_config.scale_b         = current_scale_b;
+simulation_config.alphaconst      = costfunction_alphaconst;
+simulation_config.alpha           = current_alpha;
+simulation_config.trials          = trial;
+simulation_config.use_boneportion = use_boneportion;
+simulation_config.noise.level     = noise_level;
+simulation_config.noise.tconst    = noise_tconst;
+simulation_config.noise.skewconst = noise_skewconst;
+simulation_config.noise.Rconst    = noise_Rconst;
+
+% save the necessary files
+filename = sprintf('abmodeparamsearch_%d_%d_%d.mat', current_scale_a, current_scale_b, current_alpha*10);
+fullpath = strcat(filepath, filesep, filename);
+save(fullpath, 'costfunctions_min', 'simulation_config');
+
+% end alpha
+end
+% end scale_b
+end
+% end scale_a
+end
 
 
 
